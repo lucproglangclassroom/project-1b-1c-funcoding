e59d4a7291fefeb6ee80287caf779a8fc82d8a37
Joelmesa <119980617+Joelmesa@users.noreply.github.com>
2026-02-13 15:57:23 -0600

    Person B: add CLI parsing, logging, wiring, packaging

--- build.sbt
+++ build.sbt
@@ -1,9 +1,11 @@
-name := "echotest-scala"
+name := "topwords"
 
 version := "0.4"
 
 libraryDependencies ++= Seq(
   "com.lihaoyi"       %% "mainargs"        % "0.7.8",
+  "org.slf4j"         %  "slf4j-api"       % "2.0.13",
+  "ch.qos.logback"    %  "logback-classic" % "1.5.6",
   "com.github.sbt.junit" % "jupiter-interface" % "0.17.0" % Test, // required only for plain JUnit testing
   "org.scalatest"     %% "scalatest"       % "3.2.19"   % Test,
   "org.scalacheck"    %% "scalacheck"      % "1.19.0"   % Test,
--- src/main/scala/topwords/Main.scala
+++ src/main/scala/topwords/Main.scala
@@ -1,38 +1,54 @@
 package topwords
 
-import scala.io.Source
-import scala.util.Using
+import mainargs.*
+import org.slf4j.LoggerFactory
+import scala.util.control.NonFatal
 
-object Main {
+object Main:
 
-  def main(args: Array[String]): Unit = {
+  private val log = LoggerFactory.getLogger(getClass)
 
-    // Default values
-    val cloudSize = 10
-    val minLength = 6
-    val windowSize = 1000
+  @main
+  case class Args(
+    @arg(name = "cloud-size", short = 'c', doc = "number of words to show in the cloud")
+    cloudSize: Int = 10,
 
-    val observer = new ConsoleObserver
-    val engine = new TopWordsEngine(
-      cloudSize,
-      minLength,
-      windowSize,
-      observer
-    )
+    @arg(name = "length-at-least", short = 'l', doc = "minimum word length to be counted")
+    minLength: Int = 6,
 
-    val lines = Source.stdin.getLines()
+    @arg(name = "window-size", short = 'w', doc = "moving window size of recent qualifying words")
+    windowSize: Int = 1000
+  )
 
-    val words =
-      lines.flatMap(line =>
-        line.split("(?U)[^\\p{Alpha}0-9']+")
-      )
+  def main(args: Array[String]): Unit =
+    val a = ParserForClass[Args].constructOrExit(args)
 
-    try {
+    if a.cloudSize <= 0 || a.minLength <= 0 || a.windowSize <= 0 then
+      System.err.println("All arguments must be positive.")
+      sys.exit(2)
+
+    log.debug(s"howMany=${a.cloudSize} minLength=${a.minLength} lastNWords=${a.windowSize}")
+
+    val observer: CloudObserver = ConsoleObserver()
+    val engine = new TopWordsEngine(a.cloudSize, a.minLength, a.windowSize, observer)
+
+    try
+      val lines = scala.io.Source.stdin.getLines
+      val words =
+        import scala.language.unsafeNulls
+        lines
+          .flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+"))
+          .iterator
+          .filter(_.nonEmpty)
+
       words.foreach(engine.processWord)
-    } catch {
-      case _: java.io.IOException =>
-        // Graceful exit for broken pipe
-        ()
-    }
-  }
-}
+    catch
+      case e: java.io.IOException if isBrokenPipe(e) =>
+        sys.exit(0)
+      case NonFatal(e) =>
+        log.error("Fatal error", e)
+        sys.exit(1)
+
+  private def isBrokenPipe(e: java.io.IOException): Boolean =
+    val msg = Option(e.getMessage).getOrElse("")
+    msg.contains("Broken pipe") || msg.contains("EPIPE")
