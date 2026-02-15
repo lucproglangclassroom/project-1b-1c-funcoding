ba7e012165ee7bb5ce7c5562aec09a79ccfbe2f8
Mohammed Khursiwala <mkhursiwala@luc.edu>
2026-02-15 12:30:59 -0600

    Project 1b complete.
    - Sliding-window word cloud engine
    - Deterministic output
    - CLI parsing + validation
    - Logging via SLF4J/Logback
    - Automated test suite
    - Coverage report generated
    - Scalability validated
    - README + LLM documentation included

--- src/main/scala/topwords/Main.scala
+++ src/main/scala/topwords/Main.scala
@@ -1,54 +1,78 @@
 package topwords
 
-import mainargs.*
 import org.slf4j.LoggerFactory
 import scala.util.control.NonFatal
 
-object Main:
+object Main {
 
   private val log = LoggerFactory.getLogger(getClass)
 
-  @main
-  case class Args(
-    @arg(name = "cloud-size", short = 'c', doc = "number of words to show in the cloud")
-    cloudSize: Int = 10,
+  // If mainargs is not on the classpath yet, this still compiles and runs with defaults.
+  // (Teammate can re-add @main + ParserForClass after build.sbt deps are fixed.)
+  final case class Args(
+                         cloudSize: Int = 10,
+                         minLength: Int = 6,
+                         windowSize: Int = 1000
+                       )
 
-    @arg(name = "length-at-least", short = 'l', doc = "minimum word length to be counted")
-    minLength: Int = 6,
+  def main(args: Array[String]): Unit = {
+    val a = parseArgs(args)
 
-    @arg(name = "window-size", short = 'w', doc = "moving window size of recent qualifying words")
-    windowSize: Int = 1000
-  )
-
-  def main(args: Array[String]): Unit =
-    val a = ParserForClass[Args].constructOrExit(args)
-
-    if a.cloudSize <= 0 || a.minLength <= 0 || a.windowSize <= 0 then
+    if (a.cloudSize <= 0 || a.minLength <= 0 || a.windowSize <= 0) {
       System.err.println("All arguments must be positive.")
       sys.exit(2)
+    }
 
     log.debug(s"howMany=${a.cloudSize} minLength=${a.minLength} lastNWords=${a.windowSize}")
 
-    val observer: CloudObserver = ConsoleObserver()
+    val observer: CloudObserver = new ConsoleObserver
     val engine = new TopWordsEngine(a.cloudSize, a.minLength, a.windowSize, observer)
 
-    try
+    try {
       val lines = scala.io.Source.stdin.getLines
       val words =
-        import scala.language.unsafeNulls
         lines
-          .flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+"))
-          .iterator
+          .flatMap(_.split("(?U)[^\\p{Alpha}0-9']+"))
           .filter(_.nonEmpty)
 
       words.foreach(engine.processWord)
-    catch
+    } catch {
       case e: java.io.IOException if isBrokenPipe(e) =>
         sys.exit(0)
       case NonFatal(e) =>
         log.error("Fatal error", e)
         sys.exit(1)
+    }
+  }
 
-  private def isBrokenPipe(e: java.io.IOException): Boolean =
+  def isBrokenPipe(e: java.io.IOException): Boolean = {
     val msg = Option(e.getMessage).getOrElse("")
     msg.contains("Broken pipe") || msg.contains("EPIPE")
+  }
+
+  def parseArgs(args: Array[String]): Args = {
+    // supports: --cloud-size N, -c N, --length-at-least N, -l N, --window-size N, -w N
+    def nextInt(i: Int): Option[Int] =
+      if (i + 1 < args.length) args(i + 1).toIntOption else None
+
+    var cloud = 10
+    var minL = 6
+    var win = 1000
+
+    var i = 0
+    while (i < args.length) {
+      args(i) match {
+        case "--cloud-size" | "-c" =>
+          nextInt(i).foreach { v => cloud = v }; i += 2
+        case "--length-at-least" | "-l" =>
+          nextInt(i).foreach { v => minL = v }; i += 2
+        case "--window-size" | "-w" =>
+          nextInt(i).foreach { v => win = v }; i += 2
+        case _ =>
+          i += 1
+      }
+    }
+
+    Args(cloud, minL, win)
+  }
+}
